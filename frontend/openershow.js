a00edc41966f9306429c83b4025ac2928b89ba56
dfert1987 <github email address>
2020-05-07 10:54:47 -0600

    stats for openers added

--- frontend/openershow.js
+++ frontend/openershow.js
@@ -3,7 +3,12 @@ const id = searchParams.get('id')
 const input = document.querySelector("#headliner-id")
 input.value = id
 
-
+// const lengthSelect = document.querySelector("select[name=length")
+// const poliSelect = document.querySelector("select[name=political")
+// const controSelect = document.querySelector("select[name=controversial")
+// const dirtySelect = document.querySelector("select[name=dirty")
+// const opinionSelect = document.querySelector("select[name=opinionated")
+// const ratings = [1, 2, 3, 4, 5]
 
 fetch(`http://localhost:3000/headliners/${id}`)
     .then(response => response.json())
@@ -18,12 +23,14 @@ function displayHeader(headliner) {
     const h2 = document.createElement('h2')
     h2.innerText = `If you like ${headliner.name}, you might also like ...`
     document.body.append(h2)
+    return opener
 }
 
 function getOpeners(openers) {
     openers.forEach(opener => {
        if (opener.headliner_id == id)  { 
        const showOpener = document.createElement('h3')
+    //    showOpener.innerText = `${opener.name}`
        showOpener.innerHTML= `<a href="openerwork.html?id=${opener.id}">${opener.name}</a>`
        console.log(opener.id)
        document.body.append(showOpener)
--- frontend/openerwork.js
+++ frontend/openerwork.js
@@ -10,13 +10,13 @@ fetch(`http://localhost:3000/openers/${id}`)
   
     
     function getOpenerApi(opener) {
-        console.log(opener)
         fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q=artist:"${opener.name}"`)
         .then(response => response.json())
         .then(artist => {
             console.log(artist)
             getOpenerPic(artist.data[0].picture_medium)
             getURL(artist.data[0].link)
+                    displayOpenerStats(opener)
 
         })
     }
@@ -24,7 +24,7 @@ fetch(`http://localhost:3000/openers/${id}`)
     function getURL(opener) {
         const link = document.createElement('h2')
         link.innerText = "artist page"
-        link.innerHTML = `<a href="${opener}">Check Out Comedian Page</a>`
+        link.innerHTML = `<a href="${opener}">Check out Page</a>`
         document.body.append(link)
     }
 
@@ -41,4 +41,19 @@ fetch(`http://localhost:3000/openers/${id}`)
         h2.innerText = `Here's some stuff by ${opener.name}`
         document.body.append(h2)
         return opener
+
     }
+
+    function displayOpenerStats(opener) {
+        const header = document.createElement('h3')
+        const subHeader = document.createElement('p')
+        const stats = document.createElement('h4')
+        header.innerText = "Comedy Stats"
+        subHeader.innerText = "On a scale of 1-5"
+        stats.innerText = `Length: ${opener.length}, Political leaning (lower is more liberal): ${opener.political}, Controversial: ${opener.controversial}, Dirty: ${opener.dirty}, Opinionated: ${opener.opinionated}`
+        document.body.append(header, subHeader, stats)
+        
+
+        
+    }
+    
